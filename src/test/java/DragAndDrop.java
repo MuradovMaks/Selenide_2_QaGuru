8f8bdb71039cb673dee7054b73c843aa07371df9
Maxim Muradov <koreantech620@mail.ru>
2024-04-26 14:24:58 +0700

    Refactoring Tests

--- src/test/java/DragAndDrop.java
+++ src/test/java/DragAndDrop.java
@@ -1,22 +1,22 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop
 {
     @Test
-    public void DragsAndDrop()
+    public void dragsAndDropTest()
     {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         SelenideElement element = $("#column-a");
         SelenideElement element2 = $("#column-b");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         actions().moveToElement(element).clickAndHold().moveToElement(element2).release().perform();
-        element.shouldHave(Condition.text("B"));
-        element2.shouldHave(Condition.text("A"));
-        sleep(5000);
+        element.shouldHave(text("B"));
+        element2.shouldHave(text("A"));
 
     }
 }
--- src/test/java/SearchFindestPage.java
+++ src/test/java/SearchFindestPage.java
@@ -1,19 +1,15 @@
 import org.junit.jupiter.api.Test;
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 public class SearchFindestPage
 {
     @Test
-    void FindestPage()
+    void findestPageTest()
     {
         open("https://github.com/");
-        $("ul li:nth-child(2) > button").hover();
+        $(byTagAndText("button","Solutions")).hover();
         $("[href='/enterprise']").click();
         $(".application-main").shouldHave(text("To build, scale, and deliver secure software."));
-
-        sleep(5000);
     }
 }
